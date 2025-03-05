e0eb5da3e22d1d9718896f2a46e7f43171288b26
Devasshish <devashishmac13@gmail.com>
2025-03-05 21:16:49 +0530

    Updated files

---
+++ posts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import home, create_post, like_post,about,contact,add_comment,save_post,saved_posts
+from . import views
+urlpatterns = [
+    path('', home, name='home'),
+    path('about/', about, name='about'), 
+    path('contact/', contact, name='contact'),
+    path('create/', create_post, name='create_post'),
+    path('like/<int:post_id>/', like_post, name='like_post'),
+    path('save/<int:post_id>/', save_post, name='save_post'),
+    path('saved/', saved_posts, name='saved_posts'),
+    path('post/<int:post_id>/comment/', views.add_comment, name='add_comment'),
+    path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    path('help/', views.help, name='help'),
+]
